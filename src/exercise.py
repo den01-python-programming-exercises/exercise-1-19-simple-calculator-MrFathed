2f5d40c0a35fe3d2e629924c90e3be97851da3bd
Justin Turner <jmturner638@gmail.com>
2021-07-26 03:30:48 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
+    first = int(input("Give the first number:"))
+    second = int(input("Give the second number:"))
+
+    sum = first + second
+    difference = first - second
+    product = first * second
+    quotient = first / second
+
+    print(str(first) + " + " + str(second) + " = " + str(sum))
+    print(str(first) + " - " + str(second) + " = " + str(difference))
+    print(str(first) + " * " + str(second) + " = " + str(product))
+    print(str(first) + " / " + str(second) + " = " + str(quotient))
 
 if __name__ == '__main__':
     main()
